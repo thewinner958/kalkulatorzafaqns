c82e53cdc43db5db07a641083a4ed2c4adcaf925
TheWinner <thewinner958@gmail.com>
2019-05-04 15:24:08 +0300

    `finalised the code for once and for all

--- Kalkulatorzafaqns-Project/Kalkulatorzafaqns.vb
+++ Kalkulatorzafaqns-Project/Kalkulatorzafaqns.vb
@@ -1,6 +1,8 @@
 ﻿Public Class Kalkulatorzafaqns
 
     Private Sub btnCalculate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCalculate.Click
-        lblOutput = 2 * (Val(txtBathWidth.Text) + Val(txtBathLenght.Text))
+        lblOutput.Text =
+            Format((2 * (Val(txtBathWidth.Text) + Val(txtBathLenght.Text)) _
+            * Val(txtBathHeight.Text)) - Val(txtDoorWidth.Text) * Val(txtDoorHeight.Text), "#####.## 'кв.м'")
     End Sub
 End Class
